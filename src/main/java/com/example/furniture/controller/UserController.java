813942e00ade70edd0175284ea86aa6bd1275e7c
jhonny212 <jhonnygarcia201830454@cunoc.edu.gt>
2022-03-30 11:48:35 -0600

    .

--- src/main/java/com/example/furniture/controller/UserController.java
+++ src/main/java/com/example/furniture/controller/UserController.java
@@ -21,7 +21,8 @@ public class UserController {
 		JWTAuthorizationFilter jwtaf = new JWTAuthorizationFilter();
 		Profile profile = this.userRepository.findByUsername(username);
 		if(profile != null){
-			if(profile.getPassword().equals(pwd) && profile.status){
+			System.out.println(profile.toString());
+			if(profile.getPassword().equals(pwd)){
 				response.put("token",jwtaf.getJWTToken(profile.getUsername(),profile.getUserType(),profile.getId()));
 			}else{
 				response.put("msj","La contraseña ingresada no es la correcta");
